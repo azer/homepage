059b844ccb83ba887ad7bffb1bb447bd73ad61a5
E.Azer Koçulu <azer@kodfabrik.com>
2018-07-18 16:46:11 +0800

    Update the photo gallery

--- generate-album.js
+++ generate-album.js
@@ -2,32 +2,36 @@ const album = require("flickr-photoset-info")
 const fs = require("fs")
 const key = process.env.KEY
 
-if (!key) throw new Error('API Key needed.')
+if (!key) throw new Error("API Key needed.")
 
-console.log('Generating posts/photos.json...')
-album('72157686569836570', { key: key }, (err, photos) => {
+console.log("Generating posts/photos.json...")
+album("72157686569836570", { key: key }, (err, photos) => {
   if (err) throw err
-  fs.writeFile('./posts/photos.json', JSON.stringify(photos.map(photo)), err => {
-    if (err) throw err
-    console.log('Done!')
-  })
+  fs.writeFile(
+    "./posts/photos.json",
+    JSON.stringify(photos.map(photo)),
+    err => {
+      if (err) throw err
+      console.log("Done!")
+    }
+  )
 })
 
-function photo (props) {
+function photo(props) {
   return {
-    path: '/photo/' + props.id,
+    path: "/photo/" + props.id,
     title: props.title,
     sizes: {
-      xxlarge: size(props.sizes.large2048),
-      xlarge: size(props.sizes.large1600),
-      large: size(props.sizes.large),
-      medium: size(props.sizes.medium800),
-      small: size(props.sizes.small320)
+      xxlarge: size(props.sizes.large2048 || props.sizes.large1600, "2048"),
+      xlarge: size(props.sizes.large1600, "1600"),
+      large: size(props.sizes.large, "large"),
+      medium: size(props.sizes.medium800, "800"),
+      small: size(props.sizes.small320, "320")
     }
   }
 }
 
-function size (props) {
+function size(props, key) {
   return {
     url: props.source,
     width: props.width,
