cfff7f19e576c7955b33da73b9705fc8771d272c
E.Azer Koçulu <azer@kodfabrik.com>
2019-04-20 14:19:22 +0200

    Import nested css from index module

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,14 @@
-import React, { Component }  from 'react'
-import Menu from '../components/menu'
-import './base.css'
+import React, { Component } from "react"
+import Menu from "../components/menu"
+import "./base.css"
+import "./fonts/calibre.css"
+import "./fonts/tiempos.css"
+import "./fonts/pt-mono.css"
+import "./font-definitions.css"
+import "./zigzag.css"
 
 const Container = ({ children }) => {
-  return (
-    children()
-  )
+  return children()
 }
 
 export default Container
